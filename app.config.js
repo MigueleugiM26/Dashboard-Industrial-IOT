bdefc741174dc2455bbcd36d4e18cad59611ef6c
Miguel Gabriel <srtjmg@gmail.com>
2025-10-06 21:12:58 -0300

    Fix ícones

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "Dashboard Industrial",
     slug: "dashboard-industrial",
     owner: "dashboardindustrialestacio",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
